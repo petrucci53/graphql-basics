8cb45c0b8b9a47c320b35f2be36cde95e8d4ee36
Furkan <fgencer12@gmail.com>
2019-06-30 20:59:55 +0300

    Use Spread Operator

--- src/index.js
+++ src/index.js
@@ -165,9 +165,7 @@ const resolvers = {
             }
             const user = {
                 id: uuidv4(), // new random id
-                name: args.name,
-                email: args.email,
-                age: args.age
+                ...args
             }
             users.push(user)
 
@@ -182,10 +180,7 @@ const resolvers = {
             }
             const post = {
                 id: uuidv4(), // new random id
-                title: args.title,
-                body: args.body,
-                published: args.published,
-                author: args.author
+                ...args
             }
             posts.push(post)
 
@@ -199,9 +194,7 @@ const resolvers = {
             }
             const comment = {
                 id: uuidv4(), // new random id
-                text: args.text,
-                author: args.author,
-                post: args.post
+                ...args
             }
             comments.push(comment)
 
